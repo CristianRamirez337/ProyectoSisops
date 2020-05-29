f761a6e85edee3b529dd4a00fd07e07c81bd95b3
CristianRamirez337 <CristianRamirez337@users.noreply.github.com>
2020-05-29 02:12:26 -0500

    Server now makes a tread for every message it receives

--- Server.py
+++ Server.py
@@ -1,45 +1,92 @@
 '''
-    Proyecto estacionamiento Sistemas Operativos
+    Proyecto estacionameinto Sistemas Operativos
 
     Cristian Aurelio Ramírez Anzaldo A01066337
 '''
 
 import socket
 import sys
-import time
+import threading
+from time import gmtime, strftime, sleep, time, thread_time_ns
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Bind the socket to the port
-server_address = ('localhost', 8888)
-print('starting up on %s port %s' % server_address)
-sock.bind(server_address)
+def establish_connection():
+    '''
+        Function to Establish connection with the client
+        Returns: sock.accept() - connection and client address
+    '''
 
-# Listen for incoming connections
-sock.listen(1)
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print('waiting for a connection')
-connection, client_address = sock.accept()
+    # Bind the socket to the port
+    server_address = ('localhost', 8888)
+    print('starting up on %s port %s' % server_address)
+    sock.bind(server_address)
 
-try:
-    print('connection from', client_address)
+    # Listen for incoming connections
+    sock.listen(1)
 
-    # Receiving the data
-    while True:
-        data = connection.recv(256)
-        print('server received "%s"' % data.decode('utf-8'))  # data bytes back to str
-        if data:
-            print('sending answer back to the client')
+    print('waiting for a connection')
+    return sock.accept()
 
-            connection.sendall(b'va de regreso...' + data)  # b converts str to
-            # bytes
-        else:
-            print('no data from', client_address)
-            connection.close()
-            sys.exit()
 
-finally:
-    # Clean up the connection
-    print('se fue al finally')
-    connection.close()
+def data_processor(message):
+    print("Thread says: " + message)
+
+
+def main():
+    ''' Main function of the parking lot system'''
+
+    # --------/ Variables \--------
+    # Dictionary to store table data
+    server_data_table = {'Time-stamp': [], 'Command': [], 'Server display': [], 'Free': [], 'Occupied': []}
+
+    # Counters
+    free_places_var = 50
+    occupied_places_var = 0
+
+    # Buffer semaphores
+    free_places = threading.Semaphore(value=free_places_var)
+    occupied_places = threading.Semaphore(value=occupied_places_var)
+
+    # Establishing connection with the server
+    connection, client_address = establish_connection()
+
+    try:
+        print('connection from', client_address)
+
+        # Getting time from the processor
+        current_time = strftime("%a, %d %b %Y %H:%M:%S 0", gmtime())
+        print("Starting time is: " + current_time)
+
+
+
+        # Receiving the data
+        while True:
+            data = connection.recv(256)
+            print('server received "%s"' % data.decode('utf-8'))  # data bytes back to str
+
+            if data:
+                client_message = data.decode('utf-8')
+
+                t = threading.Thread(target=data_processor, args=(client_message,))
+                t.start()
+
+                connection.sendall(b'data received...')
+
+                server_data_table['Time-stamp'].append(client_message[:client_message.find(' ')])
+            else:
+                print('no data from', client_address)
+                connection.close()
+                sys.exit()
+
+    finally:
+        # Clean up the connection
+        print('se fue al finally')
+        print(server_data_table)
+        connection.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ clientTest.py
@@ -0,0 +1,36 @@
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 8888)
+print('connecting to %s port %s' % server_address)
+sock.connect(server_address)
+
+
+messages = ['0.00 Apertura 50 2', '1.00 oprimeBoton 1', '49.00 meteTarjeta 1']
+try:
+    # Send data
+    for m in messages:
+        print("client sending " + m)
+        sock.sendall(m.encode('utf-8'))
+
+        respuesta = sock.recv(256)
+        print('client received ' + respuesta.decode('utf-8'))
+finally:
+    print('closing socket')
+    sock.close()
+
+
+
+    def main(args):
+        return 0
+
+
+    if __name__ == '__main__':
+        import sys
+
+        sys.exit(main(sys.argv))
+
