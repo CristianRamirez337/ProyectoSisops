264c88c5460ee07b604512e3f5e14f9eb7b904ae
CristianRamirez337 <CristianRamirez337@users.noreply.github.com>
2020-05-29 00:20:58 -0500

    Server can receive message bia socket

--- Server.py
+++ Server.py
@@ -1,5 +1,6 @@
 '''
-    Proyecto estacionameinto Sistemas Operativos
+    Proyecto estacionamiento Sistemas Operativos
+
     Cristian Aurelio Ramírez Anzaldo A01066337
 '''
 
@@ -7,3 +8,38 @@ import socket
 import sys
 import time
 
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the port
+server_address = ('localhost', 8888)
+print('starting up on %s port %s' % server_address)
+sock.bind(server_address)
+
+# Listen for incoming connections
+sock.listen(1)
+
+print('waiting for a connection')
+connection, client_address = sock.accept()
+
+try:
+    print('connection from', client_address)
+
+    # Receiving the data
+    while True:
+        data = connection.recv(256)
+        print('server received "%s"' % data.decode('utf-8'))  # data bytes back to str
+        if data:
+            print('sending answer back to the client')
+
+            connection.sendall(b'va de regreso...' + data)  # b converts str to
+            # bytes
+        else:
+            print('no data from', client_address)
+            connection.close()
+            sys.exit()
+
+finally:
+    # Clean up the connection
+    print('se fue al finally')
+    connection.close()
