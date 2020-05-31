feeceaab128c04edc9f028e61b5210d0d865b8b1
CristianRamirez337 <CristianRamirez337@users.noreply.github.com>
2020-05-30 21:08:07 -0500

    More instructions added to client

--- clientTest.py
+++ clientTest.py
@@ -9,7 +9,10 @@ server_address = ('localhost', 8080)
 print('connecting to %s port %s' % server_address)
 sock.connect(server_address)
 
-messages = ['0.00 Apertura 50 2 2', '1.00 oprimeBoton 1', '49.00 meteTarjeta 1', '29.0 oprimeBoton 1', '55.00 Cierre']
+messages = ['0.00 Apertura 50 2 2', '1.00 oprimeBoton 1', '19.00 recogeTarjeta 1', '21.00 laserOffE 1', '22.00 laserOnE 1',
+            '29.00 oprimeBoton 1', '32.00 oprimeBoton 2', '44.00 recogeTarjeta 1', '47.00 recogeTarjeta 2', 
+            '49.00 meteTarjeta 1 1', '52.00 laserOffE 1', '53.00 laserOnE 1', '55.00 laserOffE 2', '56.00 laserOnE 2', 
+            '57.00 laserOffS 1', '58.00 LaserOnS 1', '60.00 Cierre']
 try:
     # Send data
     for m in messages:
